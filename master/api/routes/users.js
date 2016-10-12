30429812250690bbf215f3edaa1a2bbefafcef20
hardiskbohra <hardiskbohra@gmail.com>
2016-10-12 20:45:53 +0530

    adding running mode

--- master/api/routes/users.js
+++ master/api/routes/users.js
@@ -4,33 +4,21 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    
-    
-    
   MongoClient.connect("mongodb://dishant:123456@ds053196.mlab.com:53196/coursegame", function(err, db) {
   if(!err) {
     console.log("We are connected");
-      
+
         var collection = db.collection('User');
         var doc1 = {'hello':'doc1'};
         var doc2 = {'hello':'doc2'};
         collection.insert(doc1);
-      collection.find().toArray(function(err, items) {
+        collection.find().toArray(function(err, items) {
           res.send(items);
       });
-      
-      
+
+
   }
 });
-    
-    
-    
-    
 });
-
-router.get('/name',function(req,res)
-          {
-    res.send("Hello");
-})
 
 module.exports = router;
