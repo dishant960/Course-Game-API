b28edfa81bb0023c141250c66e0ae0d46d88a68e
Hardik thakkar <hrt7809@gmail.com>
2016-10-21 16:35:08 +0530

    Commit login and resgitration

--- master/WebInterface/login/login.controller.js
+++ master/WebInterface/login/login.controller.js
@@ -5,31 +5,31 @@ app.controller("loginctrl",function($scope,$resource,$state){
     {
         
 
-        // var uname = $scope.user.username;
-        // var password = $scope.user.password;
+        var uname = $scope.user.username;
+        var password = $scope.user.password;
      
         
-        // if (uname == 'hardik' && password == '123') 
-        // {
-        //         alert('match');
-        // }
-        // else
-        // {
-        //     alert('not match');
-        // }
+        if (uname == 'hardik' && password == '123') 
+        {
+                alert('match');
+        }
+        else
+        {
+            alert('not match');
+        }
         
 
-a=$resource("https://jsonplaceholder.typicode.com/users");
-        a.save($scope.user,function(res)
-        {
-            if(res.value == "s")
-            {
-                    $state.go("home");
-                }
-            else
-                {
+// a=$resource("https://jsonplaceholder.typicode.com/users");
+//         a.save($scope.user,function(res)
+//         {
+//             if(res.value == "s")
+//             {
+//                     $state.go("home");
+//                 }
+//             else
+//                 {
                     
-                }
+//                 }
 
-    }
+//     }
 });
