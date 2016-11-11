36fa3b9179c1e260b74aa09becf3f7acf30f53e2
Jaspreet Chhabra <jaspreetchhabra94@gmail.com>
2016-11-12 04:24:33 +0530

    deleted all files

--- master/WebInterface/emnem.js
+++
@@ -1,8 +0,0 @@
-var user = $scope.user;
-        console.log($scope.user);
-        
-        $scope.user = { "username" : "bjscdb", "password" =}
-        var a=$resource("localhost:3000/users/login");
-        a.save($scope.user,function(res){
-            console.log(JSON.stringfy(res));
-        });
